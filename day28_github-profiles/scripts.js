e49433d994a44aa8f0b530c1e08979c0592bf803
Chris Owens <guitarrockstarco@gmail.com>
2021-01-16 13:06:22 -0500

    Day 28 Done

--- day28_github-profiles/scripts.js
+++ day28_github-profiles/scripts.js
@@ -8,10 +8,20 @@ async function getUser(username) {
     try {
         const { data } = await axios(APIURL + username);
         createUserCard(data);
+        getRepos(username);
     } catch (err) {
         if (err.response.status == 404) {
             createErrorCard('No profile with this username');
         }
+    }
+}
+
+async function getRepos(username) {
+    try {
+        const { data } = await axios(APIURL + username + '/repos?sort=created');
+        addReposToCard(data);
+    } catch (err) {
+        createErrorCard('Problem fetching repos');
     }
 }
 
@@ -40,7 +50,7 @@ function createUserCard(user) {
     main.innerHTML = cardHTML;
 }
 
-function createErrorCar(msg) {
+function createErrorCard(msg) {
     const cardHTML = `
     <div class="card>
         <h1>${msg}</h1>
@@ -48,6 +58,19 @@ function createErrorCar(msg) {
     `;
 
     main.innerHTML = cardHTML;
+}
+
+function addReposToCard(repos) {
+    const reposEl = document.getElementById('repos');
+    repos.slice(0, 10).forEach((repo) => {
+        const repoEl = document.createElement('a');
+        repoEl.classList.add('repo');
+        repoEl.href = repo.html_url;
+        repoEl.target = '_blank';
+        repoEl.innerText = repo.name;
+
+        reposEl.appendChild(repoEl);
+    });
 }
 
 form.addEventListener('submit', (e) => {
